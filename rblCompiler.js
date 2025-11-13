06beda8e2a71e5e2cd664f3ce79d9aeb95beea0e
shivmoh <shiveshmohamed1234@gmail.com>
2025-11-13 19:29:04 -0400

    changes

--- rblCompiler.js
+++ rblCompiler.js
@@ -7,42 +7,6 @@ const map = require('./componentMap');
 // check if element is a child element
 // if yes, add child element to previous element
 // if not, add element at top layer
-
-function isChildElement(tabCount) {
-    if (tabCount > 0) return true;
-    return false;
-}
-
-
-async function getFileElements(files) {
-    var promises = [];
-    var directory_path = './test';
-
-    for (const file of files) {
-        const file_path = path.join(directory_path, file);
-        var counter = 0;
-
-        const read_interface = readline.createInterface({
-            input: fs.createReadStream(file_path),
-            output: null,
-            console: false
-        });
-
-        await read_interface.on('line', async (line) => {
-            const elementName = line.replaceAll(' ', '');
-            const tabCount = line.match(/^\t+/);
-                
-            if (map.has(elementName)) {
-                var element = await map.get(elementName).createElement(); // store the Promise
-                promises.push(element);
-            }
-        });
-    };
-
-    console.log('hello test')
-    return "testing";
-}
-
 async function getElements() {
     const directory_path = './test';
     const files = await fs.promises.readdir(directory_path);
@@ -51,7 +15,9 @@ async function getElements() {
     for(const file of files) {
         const file_path = path.join(directory_path, file);
         const file_content = await fs.promises.readFile(file_path, {encoding: 'utf-8'});
+        var base_element = "";
         var element_build = "";
+        var previous_tab_count = 0;
 
         var elements = file_content.split('\n');
 
@@ -68,10 +34,18 @@ async function getElements() {
                         ret_elements.push(element_build);
                     }
                     
+                    previous_tab_count = tab_count + 1;
+                    base_element = html_element;
                     element_build = html_element;
-                
                 } else {
-                    var split = element_build.split('<!--SPLIT-->');
+
+                    if (tab_count > previous_tab_count) { 
+                        previous_tab_count = tab_count; 
+                        base_element = previous_element;
+                    }
+
+                    var split = element_build
+                        .split('<!--SPLIT-->');
                     var insert_index = (split.length / 2);
                     element_build = split
                         .splice(insert_index, 0, html_element)
